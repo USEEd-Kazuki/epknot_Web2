6233dd86c5db381f6e03a14154bc928f6c543150
Kazuki <kazuone0321@icloud.com>
2025-06-01 18:32:53 +0900

    TEST

--- src/components/FixCom.js
+++ src/components/FixCom.js
@@ -11,6 +11,7 @@ const FixCom = () => {
         <div className="FixCompaney-message">
           <p>
           弊社はこれまでに70隻以上のSLB案件の成約に至っており、これはファイナンス市場規模で置き換えると1,500億円程度となります。<br/>年間平均では10隻程度の案件成約となっております。
+          TEST
           </p>
         </div>
         </div>
